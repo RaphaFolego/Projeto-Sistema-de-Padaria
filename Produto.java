98c32b545306a87155c547382cbecf661974c87e
Raphael Miguel Folego <raphael.folego@gmail.com>
2022-09-22 09:34:17 -0300

    Update Produto.java

--- Produto.java
+++ Produto.java
@@ -15,18 +15,38 @@ public class Produto {
 	//constructors
 	
 	//getters and setters
-	public void getPreco(double preco) {
-		System.out.println("O valor do produto é -->" + preco);
+	public String setId() {
+		return this.Id;
+	}
+	public String setNome() {
+		return this.nome;
+	}
+	public double setPreco() {
+		return this.preco;
+	}
+	
+	public void getPreco() {
 		this.preco = preco;
+	}
+	public void getNome() {
+		this.nome = nome;
+	}
+	public void getId() {
+		this.Id = Id;
 	}
 	
 	//methods
 	public void Alterar() {
-		
+		if(confirmar = true) {
+			System.out.println("Informe o número de Identificação do produto: ");
+			this.Id = Id;
+		}
 	}
 	public void Excluir(boolean confirmar) {
 		if(confirmar = true) {
 			System.out.println("O produto foi excluido");
+		}else{
+			System.out.println("Para cancelar a operação digite 1");		
 		}
 	}
 	public void Consultar(String Id) {
