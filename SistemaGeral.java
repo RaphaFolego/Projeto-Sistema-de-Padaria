81ba4cb5d221f6254fdb7c748c8f716dd489dc28
Raphael Miguel Folego <raphael.folego@gmail.com>
2022-10-19 16:35:47 -0300

    Update SistemaGeral.java

--- SistemaGeral.java
+++ SistemaGeral.java
@@ -1,13 +1,60 @@
-package SistemaPadaria;
+package com.mycompany.padaria;
 
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
-public class SistemaGeral {
+public class Padaria {
 
-	public static void main(String[] args) {
-		Produto paozinho = new Produto();
-		
-		paozinho.Registrar();
-		paozinho.preco();		
-	}
+    public static void main(String[] args) {
+        int op;
+        String menuPadaria = "1- Registrar produto\n2- Alterar produto\n3- Excluir produto\n4- Consultar produto\n0- Cancelar operação";
+        do{
+            op = Integer.parseInt(JOptionPane.showInputDialog(menuPadaria));
+            switch(op){
+                case 1:{
+                    String nome = JOptionPane.showInputDialog("Nome: ");
+                    Double preco = Double.parseDouble(JOptionPane.showInputDialog("Preço: "));
+                    String unidade = JOptionPane.showInputDialog("Unidade");
+                    Produto p = new Produto();
+                    p.setNome(nome);
+                    p.setPreco(preco);
+                    p.setUnidade(unidade);
+                    p.RegistrarProduto();
+                
+                    break;
+                }
+                case 2:{
+                    String nome = JOptionPane.showInputDialog("Nome: ");
+                    Double preco = Double.parseDouble(JOptionPane.showInputDialog("Preco: "));
+                    String unidade = JOptionPane.showInputDialog("Unidade: ");
+                    String Id = JOptionPane.showInputDialog("Id: ");
+                    Produto p = new Produto();
+                    p.setNome(nome);
+                    p.setPreco(preco);
+                    p.setUnidade(unidade);
+                    p.setId(Id);
+                
+                    p.AlterarProduto();
+                    break;
+                }
+                case 3:{
+                    String Id = JOptionPane.showInputDialog("Id: ");
+                    Produto p = new Produto();
+                    p.setId(Id);
+                
+                    p.ExcluirProduto();
+                    break;
+                }
+                case 4:{
+                    Produto p = new Produto();
+                    p.ConsultarProduto();
+                    break;
+                }
+                case 0:{
+                    
+                }
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção inválida");
+            }
+        }while(op != 0);
+    }
 }
