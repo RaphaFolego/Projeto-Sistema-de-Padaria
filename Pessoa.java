4299ec4d82308b96209ae46c6793ee275c917714
Raphael Miguel Folego <raphael.folego@gmail.com>
2022-10-19 16:39:01 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,66 @@
+package com.mycompany.padaria;
+
+/**
+ *
+ * @author Raphael Folego
+ */
+public class Pessoa {
+    
+    //atributos
+    private String nome;
+    private String email;
+    private String fone;
+    private int codigo;
+    
+    //construtores
+    
+    //getters & setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    
+    //métodos
+    
+    public void RegistrarPessoa(){
+        
+    }
+    public void ConsultarPessoa(){
+        
+    }
+    public void AlterarPessoa(){
+        
+    }
+    public void ExcluirPessoa(){
+        
+    }
+    
+}
